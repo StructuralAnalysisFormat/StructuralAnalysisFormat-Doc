e5e830191ff27ea5293f5498dcadfea4451f4285
Dirk Schuermans <dirk@schuermans.me>
2021-11-16 22:57:22 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,12 @@
+import importlib
+
 extensions = ['myst_parser']
 source_suffix = ['.rst', '.md']
 master_doc = 'index'
 project = u'gitbookdocumentation'
+
+theme = importlib.import_module('sphinx_rtd_theme')
+html_theme = 'sphinx_rtd_theme'
+html_style = None
 html_theme_options = {'body_max_width': '100%'}
+html_theme_path = [theme.get_html_theme_path()]
