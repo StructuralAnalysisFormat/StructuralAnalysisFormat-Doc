3648d84623aac26ef98314602f6769c6848d117b
Dirk Schuermans <dirk@schuermans.me>
2021-11-16 23:17:47 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,8 +5,5 @@ source_suffix = ['.rst', '.md']
 master_doc = 'index'
 project = u'gitbookdocumentation'
 
-theme = importlib.import_module('sphinx_rtd_theme')
 html_theme = 'sphinx_rtd_theme'
-html_style = None
-html_theme_options = {'body_max_width': '100%'}
-html_theme_path = [theme.get_html_theme_path()]
+html_style = 'custom.css'
