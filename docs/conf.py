f58e4b15aa42fa5bb2f9b7e92f2b04605588c577
SCIA nv <89633938+msalak-scia@users.noreply.github.com>
2021-12-02 13:30:54 +0100

    conf.py - change the name of project

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@ import importlib
 extensions = ['myst_parser']
 source_suffix = ['.rst', '.md']
 master_doc = 'index'
-project = u'gitbookdocumentation'
+project = u'SAF Documentation'
 
 html_css_files = [
     'custom.css',
